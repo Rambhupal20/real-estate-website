333f2dec5c24ec5e8b3940e7d99a8b6b070a6faa
Rambhupal Reddy <rambhupalreddyp102@gmail.com>
2026-08-19 16:49:48 +0530

    Your descriptive commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
-import './App.css'
 import Companies from "./Components/Companies/Companies";
 import Residencies from "./Components/Residencies/Residencies";
 import Value from "./Components/Value/Value";
 import Contact from "./Components/Contact/Contact";
 import GetStarted from "./Components/Getstarted/GetStarted";
 import Footer from "./Components/Footer/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <div className="white-gradient"/>
         <Header />
-        <Hero />
       </div>
-      <Companies />
-      <Residencies />
-      <Value />
-      <Contact />
-      <GetStarted />
+
+      <Routes>
+        {/* Default / Home View */}
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="App">
+                <div>
+                  <div className="white-gradient" />
+                  
+                  <Hero />
+                </div>
+              </div>
+              <Companies />
+              <Residencies />
+              <Value />
+              <Contact />
+              <GetStarted />
+            </>
+          }
+        />
+
+        {/* Dedicated Page Views */}
+        <Route path="/residencies" element={<Residencies />} />
+        <Route path="/value" element={<Value />} />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/get-started" element={<GetStarted />} />
+      </Routes>
+
       <Footer />
     </div>
   );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { NavLink } from "react-router-dom";
 import { BiMenuAltRight } from "react-icons/bi";
-import OutsideClickHandler from "react-outside-click-handler";
 import "./Header.css";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
+  const menuRef = useRef(null);
+
+  // Close menu on outside click
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuOpened(false);
+      }
+    };
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => document.removeEventListener("mousedown", handleOutsideClick);
+  }, []);
 
   const getMenuStyles = (opened) => {
     if (document.documentElement.clientWidth <= 768) {
@@ -12,39 +24,37 @@ const Header = () => {
     }
   };
 
-  const handleLinkClick = () => {
-    setMenuOpened(false);
-  };
+  const handleLinkClick = () => setMenuOpened(false);
 
   return (
     <section className="h-wrapper">
-      <div className="flexCenter paddings innerWidth h-container">
+      <div className="flexCenter paddings innerWidth h-container" ref={menuRef}>
         {/* Brand Logo */}
-        <img src="./logo.png" alt="logo" width={100} className="h-logo" />
+        <NavLink to="/" onClick={handleLinkClick}>
+          <img src="./logo.png" alt="logo" width={100} className="h-logo" />
+        </NavLink>
 
         {/* Navigation Menu */}
-        <OutsideClickHandler onOutsideClick={() => setMenuOpened(false)}>
-          <div
-            className={`flexCenter h-menu ${menuOpened ? "menu-opened" : ""}`}
-            style={getMenuStyles(menuOpened)}
-          >
-            <a href="#residencies" onClick={handleLinkClick}>
-              Residencies
-            </a>
-            <a href="#value" onClick={handleLinkClick}>
-              Our Value
-            </a>
-            <a href="#contact-us" onClick={handleLinkClick}>
-              Contact Us
-            </a>
-            <a href="#get-started" onClick={handleLinkClick}>
-              Get Started
-            </a>
-            <button className="button" onClick={handleLinkClick}>
-              <a href="#contact-us">Contact</a>
-            </button>
-          </div>
-        </OutsideClickHandler>
+        <div
+          className={`flexCenter h-menu ${menuOpened ? "menu-opened" : ""}`}
+          style={getMenuStyles(menuOpened)}
+        >
+          <NavLink to="/residencies" onClick={handleLinkClick}>
+            Residencies
+          </NavLink>
+          <NavLink to="/value" onClick={handleLinkClick}>
+            Our Value
+          </NavLink>
+          <NavLink to="/contact-us" onClick={handleLinkClick}>
+            Contact Us
+          </NavLink>
+          <NavLink to="/get-started" onClick={handleLinkClick}>
+            Get Started
+          </NavLink>
+          <NavLink to="/contact-us" className="button" onClick={handleLinkClick}>
+            Contact
+          </NavLink>
+        </div>
 
         {/* Mobile Hamburger Icon */}
         <div
